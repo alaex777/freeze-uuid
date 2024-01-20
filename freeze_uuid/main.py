d29d2334377beffbb90955f0c2f18bc067bc3513
alaex777 <alaex777@gmail.com>
2024-01-20 16:58:28 +0300

    Added support for uuid default methods and fields

--- freeze_uuid/main.py
+++ freeze_uuid/main.py
@@ -13,6 +13,7 @@ class FakeUUID:
     def __init__(self, *args, **kwargs) -> None:
         global COUNT
         self.value = VALUES[COUNT] if COUNT < len(VALUES) else VALUES[-1]
+        self.int = int(self.value.replace('-', ''), 16)
         COUNT += 1
 
     def __str__(self) -> str:
@@ -20,6 +21,112 @@ class FakeUUID:
 
     def __eq__(self, __value: object) -> bool:
         return str(__value) == self.value
+
+    def __lt__(self, other: object) -> bool:
+        if isinstance(other, FakeUUID):
+            return self.int < other.int
+        return NotImplemented
+
+    def __gt__(self, other: object) -> bool:
+        if isinstance(other, FakeUUID):
+            return self.int > other.int
+        return NotImplemented
+
+    def __le__(self, other: object) -> bool:
+        if isinstance(other, FakeUUID):
+            return self.int <= other.int
+        return NotImplemented
+
+    def __ge__(self, other: object) -> bool:
+        if isinstance(other, FakeUUID):
+            return self.int >= other.int
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(self.int)
+
+    def __int__(self):
+        return self.int
+    
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, str(self))
+
+    def __setattr__(self, name, value):
+        raise TypeError('UUID objects are immutable')
+    
+    @property
+    def bytes(self):
+        return self.int.to_bytes(16)  # big endian
+
+    @property
+    def bytes_le(self):
+        bytes = self.bytes
+        return (bytes[4-1::-1] + bytes[6-1:4-1:-1] + bytes[8-1:6-1:-1] +
+                bytes[8:])
+
+    @property
+    def fields(self):
+        return (self.time_low, self.time_mid, self.time_hi_version,
+                self.clock_seq_hi_variant, self.clock_seq_low, self.node)
+
+    @property
+    def time_low(self):
+        return self.int >> 96
+
+    @property
+    def time_mid(self):
+        return (self.int >> 80) & 0xffff
+
+    @property
+    def time_hi_version(self):
+        return (self.int >> 64) & 0xffff
+
+    @property
+    def clock_seq_hi_variant(self):
+        return (self.int >> 56) & 0xff
+
+    @property
+    def clock_seq_low(self):
+        return (self.int >> 48) & 0xff
+
+    @property
+    def time(self):
+        return (((self.time_hi_version & 0x0fff) << 48) |
+                (self.time_mid << 32) | self.time_low)
+
+    @property
+    def clock_seq(self):
+        return (((self.clock_seq_hi_variant & 0x3f) << 8) |
+                self.clock_seq_low)
+
+    @property
+    def node(self):
+        return self.int & 0xffffffffffff
+
+    @property
+    def hex(self):
+        return '%032x' % self.int
+
+    @property
+    def urn(self):
+        return 'urn:uuid:' + str(self)
+
+    @property
+    def variant(self):
+        if not self.int & (0x8000 << 48):
+            return uuid.RESERVED_NCS
+        elif not self.int & (0x4000 << 48):
+            return uuid.RFC_4122
+        elif not self.int & (0x2000 << 48):
+            return uuid.RESERVED_MICROSOFT
+        else:
+            return uuid.RESERVED_FUTURE
+
+    @property
+    def version(self):
+        # The version bits are only meaningful for RFC 4122 UUIDs.
+        if self.variant == uuid.RFC_4122:
+            return int((self.int >> 76) & 0xf)
 
 
 def freeze_uuid(values: Union[str, list] = DEFAULT_VALUE) -> Callable:
