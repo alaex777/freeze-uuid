04bc9be53591c505d70c5335760b693a89805736
alaex777 <alaex777@gmail.com>
2024-05-04 15:11:06 +0300

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='freeze-uuid',
-    version='0.4.2',
+    version='0.4.3',
     author='Alexander Balashov',
     author_email='alaex777@gmail.com',
     description='Python package for mocking uuid.',
